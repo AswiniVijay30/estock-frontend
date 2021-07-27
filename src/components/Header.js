dd9be9c5570a80cf226a82e0b694d62b262b0e42
AswiniVijay30 <86978327+AswiniVijay30@users.noreply.github.com>
2021-07-27 10:32:51 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Component } from 'react';
+import {Navbar,Nav} from 'react-bootstrap';
+
+class Header extends Component  {
+render(){
+  return (
+<header>
+<Navbar bg="primary" expand="lg" variant="dark">
+<span className="material-icons">
+trending_up
+</span>
+  <Navbar.Brand href="#">E-Stock Market</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Nav.Link href="#" className="text-light w-25">Add Company</Nav.Link>
+ 
+</Navbar>
+</header>
+
+  )
+}                
+}
+export default Header;
